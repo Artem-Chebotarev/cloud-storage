4e701b80748b10b816046f32cca924b9c53f0d1c
Artem Chebotarev <AChebotarev@niisokb.ru>
2023-10-30 18:48:13 -0400

    feat: files module is done

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
   app.enableCors({ credentials: true, origin: true });
+
+  // Middleware for static files
+  app.use('./uploads', express.static(join(__dirname, '..', 'uploads')));
 
   const config = new DocumentBuilder()
     .setTitle('Cloud storage')
