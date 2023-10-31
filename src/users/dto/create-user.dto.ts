a08a027d769175616a6d60ebc714c1d4b90f16b8
Artem Chebotarev <AChebotarev@niisokb.ru>
2023-10-30 21:04:44 -0400

    feat: files service is ready with delete files

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  // To specify default properties for Swagger
+  @ApiProperty({
+    default: 'test@test.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    default: 'boss',
+  })
+  fullName: string;
+
+  @ApiProperty({
+    default: '1234t*',
+  })
+  password: string;
+}
